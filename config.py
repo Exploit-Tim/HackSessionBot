aef6ec37ae7d1d5a3449e3ed606f2f6bed0e7812
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-03-07 00:02:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
+    API_ID = "25639252"
+    API_HASH = "42db0fd56c51ff2b94cf064838eba7c1"
+    TOKEN = "6879955208:AAHjbpdm9Xxlv9gKJbtiUgDFDA5AGux2fwA"  
     START_PIC = "" 
     CHAT = ""    
